000218701d1b8be78077f3f331f94c1daa149b1a
Helen Begovic <helen.begovic@gmx.de>
2018-03-25 11:09:10 +0200

    Added AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioClip gameplayTheme;
+    public AudioClip menuTheme;
+    public AudioClip menuSelection;
+    public AudioClip menuConfirmation;
+    public AudioClip pickUpPowerUp;
+    public AudioClip pickUpKey;
+    public AudioClip[] switchPlayerColor;
+    public AudioClip[] switchDoorColor;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
